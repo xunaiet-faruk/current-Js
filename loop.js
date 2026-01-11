cb6077acb31a78c160dadcec36673b8b96ffcd3e
junaietfaruk <junaietfaruk@throughltd.com>
2026-01-11 23:43:22 +0600

    coutdown loop

--- loop.js
+++ loop.js
@@ -1,14 +1,8 @@
 
 
-let i = 61;
-while(i <= 100){
-    console.log("loop is",i);
-    if(i % 2 !== 1){
-        console.log("this is even number",i);
-
-    }
-
-    i++
-
+let i =1;
+for( m =1; m<= 10; m++){
+    
+    console.log(9*m);
+ 
 }
-
