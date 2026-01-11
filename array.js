2f042be9151e188720226425e8f761072c026fdd
junaietfaruk <junaietfaruk@throughltd.com>
2026-01-11 23:08:58 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,9 @@
+
+
+const fruits = ["Apple", "Banana", "Mango"];
+const numbers = [10, 20, 30, 40];
+const isActiveList = [true, false, true];
+const name ="junaiet"
+
+
+console.log(Array.isArray(name))
---
+++ loop.js
@@ -0,0 +1,14 @@
+
+
+let i = 61;
+while(i <= 100){
+    console.log("loop is",i);
+    if(i % 2 !== 1){
+        console.log("this is even number",i);
+
+    }
+
+    i++
+
+}
+
